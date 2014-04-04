189fcb448391101c9d6aa09dd80ac514befc3935
Benjamin Pannell <admin@sierrasoftworks.com>
2014-04-04 19:08:08 +0200

    Improved cache performance for map inserts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	superagent: require('./lib/extensions/superagent-frontier'),
 	Application: require('./lib/Application'),
-	MemoryStore: require('./lib/stores/MemoryStore')
+	MemoryStore: require('./lib/stores/MemoryStore'),
+	NoOpStore: require('./lib/stores/NoOpStore')
 }
