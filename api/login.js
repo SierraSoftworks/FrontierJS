4e2ad8f664e9639127b79eac513960fbc8b529e2
Benjamin Pannell <admin@sierrasoftworks.com>
2014-10-01 16:58:37 +0200

    Implement stricter validation for login API methods

--- api/login.js
+++ api/login.js
@@ -6,7 +6,7 @@ var _ = require('lodash'),
 	fn = require('functionality');
 
 module.exports = function(frontier) {
-	frontier.login = fn.first(utils.fn.promises.first).on({ username: String, password: String }, fn.opt(Function), function(user, callback) {
+	frontier.login = fn.first(utils.fn.promises.first).on({ username: /^[a-z][a-z0-9_]{7,}$/, password: String }, fn.opt(Function), function(user, callback) {
 		this.addCallback(callback);
 
 		this.pipePromise(frontier.client.request(true, 'POST', '/api/:publickey/login', user).then(frontier.client.toBody));
